3bb57214adac5dde7b572ba3063cabf867ba8704
denischepik <gromovdenis706@gmail.com>
2026-04-03 10:15:43 +0300

    Remove exception from views

--- src/apps/posts/views.py
+++ src/apps/posts/views.py
@@ -32,15 +32,15 @@ def post_detail(request, pk):
     return render(request, "posts/post_detail.html",
                   {"post": post})
 
-def error_400(request, exception):
+def error_400(request):
     return render(request, '400.html', status=400)
 
-def error_403(request, exception):
+def error_403(request):
     return render(request, '403.html', status=403)
 
 def error_404(request, exception):
     return render(request, '404.html', status=404)
 
-def error_500(request, exception):
+def error_500(request):
     return render(request, '500.html', status=500)
 
